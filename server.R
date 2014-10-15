0bd7a932d02cc0f472115e79a63fbe7daaf79006
casm40 <casm40@gmail.com>
2014-10-15 20:17:51 +0100

    Updated Predictor App
    After some debugging

--- server.R
+++ server.R
@@ -4,70 +4,31 @@ library(shiny)
 shinyServer(function(input, output) {
         # Preload word recognition functions
         Sys.setlocale('LC_ALL','C')
-        wordrec1 <- function(x,dict,n){ 
+        ptable <- read.table("data/ptable1.txt",header=TRUE,stringsAsFactors=FALSE)
+        wordrec <- function(x,dict=ptable,n=5){ 
                 # dict is dictionary
                 # x is search string
                 # n is number to return
-                stub <- paste0("^",x)
-                words <- dict
-                result <- words[grep(stub,words$term,ignore.case = TRUE),]
-                result <- result[order(-result$count),]
-                res <- result$term[1:n]
-                res <- res[!is.na(res)]
-                res
+                library(stringr)
+                for(i in -5:-1){
+                        result <- try(word(x,i,-1),silent=TRUE)
+                        if(class(result) != "try-error") stub <- c(stub,result)
+                }
+                stub <- unique(stub)
+                for(i in 1:length(stub)){
+                        result <- ptable[grep(paste0("^",stub[i]),ptable$term,ignore.case = TRUE),]
+                        if(nrow(result) !=0) result
+                }
+                result <- result[with(result, order(-p)), ]
+                result <- result[1:n,]
+                result <- result[complete.cases(result),]
+                ifelse(nrow(result)==0,result,"Sorry! No suggestions")
+                result
         }
-        wordrec <- function(x,dict,n=5){
-                # dict is ptable
-                # x is search string
-                # n is number to return
-                stub <- paste0("^",x)
-                words <- dict
-                result <- words[grep(stub,words$term,ignore.case=TRUE),]
-                if(all(is.na(result$X5gram))){
-                        result <- result[order(-result$X5gram),]
-                }
-                else if(all(is.na(result$X4gram))){
-                        result <- result[order(-result$X4gram),]
-                }
-                else if(all(is.na(result$X3gram))){
-                        result <- result[order(-result$X3gram),]
-                }
-                else if(all(is.na(result$X2gram))){
-                        result <- result[order(-result$X2gram),]
-                }
-                else{
-                        result <- result[order(-result$X1gram),]
-                }
-                res <- result$term[1:n]
-                res <- res[!is.na(res)]
-                res
-        }
-        medterms <- read.table("data/medterms.txt",header=TRUE,stringsAsFactors =FALSE)
-        bauman <- read.table("data/bauman.txt",header=TRUE,stringsAsFactors=FALSE)
-        ptable <- read.table("data/ptable.txt",header=TRUE,stringsAsFactors=FALSE)
         # Predict next word
         output$predictedText <- renderTable({
-                if(input$radio == 1){
-                        dict <- bauman
-                        term <- as.data.frame(wordrec1(input$searchTerm,dict,input$termNumber))
-                        term <- cbind(term)
-                        colnames(term) <- "Term"
-                        term
-                }
-                else if(input$radio == 2){
-                        dict <- medterms
-                        term <- as.data.frame(wordrec1(input$searchTerm,dict,input$termNumber))
-                        term <- cbind(term)
-                        colnames(term) <- "Term"
-                        term
-                }
-                else {
-                        dict <- ptable
-                        term <- as.data.frame(wordrec(input$searchTerm,dict,input$termNumber))
-                        term <- cbind(term)
-                        colnames(term) <- "Term"
-                        term
-                }
+                result <- wordrec(input$searchTerm,dict,input$termNumber)
+                result[,1:2]
         })
 })
 
--- ui.R
+++ ui.R
@@ -10,19 +10,11 @@ shinyUI(fluidPage(
         sidebarLayout(
                 sidebarPanel(
                         div("Please wait a few moments for data loading..."),
-                        radioButtons("radio",
-                                label = h3("Choose Dictionary"),
-                                choices = list("Bauman"=1,
-                                               "Medical"=2,
-                                               "Ngram"=3),
-                                selected = 3),
+                        div("Enter your search term below."),
                         textInput("searchTerm", label = h3("Search Term"),
                           value = ""),
                         sliderInput("termNumber", label = h3("Number to Return"),
                                     min = 1, max = 25, value = 5)),
-                
-
-                # Show a plot of the generated distribution
                 mainPanel(
                         h4("Ranking by Frequency of Use"),
                         tableOutput("predictedText")
